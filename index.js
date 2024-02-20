7fb1606301b483c1bd30e4296cf958d330a3758f
Sebastian Galindo <talkysafe143@MacBook-Air-de-Valentina-3.local>
2024-02-20 15:31:12 -0500

    Adding airports to cities

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const readline = require('node:readline');
-const { stdin: input, stdout: output } = require('node:process');
-
-const rl = readline.createInterface({ input, output });
-
+const readline = require("prompt-sync")()
+const serAPI = require("serpapi")
+const fetch = require("node-fetch")
+require("dotenv").config()
 const cities = []
 
 const permutations = arr => {
@@ -17,20 +16,87 @@ const permutations = arr => {
         ),
       []
     );
-  };
-  
+};
 
-const addCity = (answer) => {
-    cities.push(answer)
+const getAirports = async city => {
+  const json = await (await fetch(`https://api.api-ninjas.com/v1/airports?city=${city}`, {
+    headers: {
+      'X-Api-Key': process.env.NINJAAPI_KEY
+    }
+  })).json();
+  return json.map(value => value.iata).filter(element => element !== '');
 }
-
-const askDates = (answer) => {
+  
+const askDates = async (answer) => {
     console.log(`Ingrese las ${answer} ciudades.`)
     for (let i = 0; i < parseInt(answer); i++) {
-        rl.question(`Ingrese el nombre de la ciudad ${i+1}: `, addCity)
+        const city = readline(`Ingrese el nombre de la ${i+1} ciudad: `);
+
+        cities.push({
+          name: city,
+          airports: await getAirports(city)
+        })
     }
 
-    cities.push()
+    console.log(cities)
 }
 
-rl.question('¿Cuantas ciudades se va a visitar?', askDates);
+
+
+(async function() {
+  const numCities = readline("¿Cuantas ciudades se van a visitar? -> ")
+
+await askDates(numCities)
+
+let dates = new Array(parseInt(numCities))
+
+console.log(`A continuación, escriba las fechas de salida y de llegada`)
+console.log(`Las fechas no cuentan la primera ciudad. Va a ingresar ${parseInt(numCities)-1} fechas.`)
+for (let i = 0; i < parseInt(numCities)-1; i++) {
+  console.log(`Ingresando la fecha ${i+1}.`)
+  const arrivalDay = readline("Ingrese el dia de llegada -> ")
+  const arrivalMonth = readline("Ingrese el mes de llegada -> ")
+  const arrivalYear = readline("Ingrese el año de llegada -> ")
+  const arrivalDate = new Date(
+    parseInt(arrivalYear), 
+    parseInt(arrivalMonth)-1, 
+    parseInt(arrivalDay)
+  );
+  console.log("--------------")
+  const departureDay = readline("Ingrese el dia de salida -> ")
+  const departureMonth = readline("Ingrese el mes de salida -> ")
+  const departureYear = readline("Ingrese el año de salida -> ")
+  const departureDate = new Date(
+    parseInt(departureYear),
+    parseInt(departureMonth)-1,
+    parseInt(departureDay)
+  );
+  
+  const departureString = departureDate.toISOString().split("T")[0];
+  const arrivalString = arrivalDate.toISOString().split("T")[0];
+
+  console.log("Las fechas ingresadas son: ")
+  console.log(`Fecha de llegada: ${arrivalString}`)
+  console.log(`Fecha de salida: ${departureString}`)
+  
+  dates[i+1] = {
+    departureString,
+    arrivalString,
+    cityName: ""
+  }
+
+  if (i == 0) {
+    dates[i] = {
+      departureString: arrivalString,
+      arrivalString: "",
+      cityName: cities[0].name
+    }
+  } 
+  
+  if (i == parseInt(numCities)-2) {
+    dates[0].arrivalString = departureString
+  }
+}
+
+console.log(dates)
+})();
